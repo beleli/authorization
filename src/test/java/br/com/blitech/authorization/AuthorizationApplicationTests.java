e893a0ccec3899f186f3fd85dbb7f2c1e1dd5033
Carlos Beleli <beleli@gmail.com>
2025-02-07 18:51:05 -0300

    sonar reviews

--- src/test/java/br/com/blitech/authorization/AuthorizationApplicationTests.java
+++ src/test/java/br/com/blitech/authorization/AuthorizationApplicationTests.java
@@ -2,7 +2,6 @@ package br.com.blitech.authorization;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 class AuthorizationApplicationTests {
