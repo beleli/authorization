58251f529bffcdc2971b6e17fc75d93fdededdc9
Carlos Beleli <beleli@gmail.com>
2025-02-13 16:33:30 -0300

    change csrf config

--- src/main/java/br/com/blitech/authorization/api/security/JwtResourceConfig.java
+++ src/main/java/br/com/blitech/authorization/api/security/JwtResourceConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -34,7 +35,7 @@ public class JwtResourceConfig {
     @Bean
     public SecurityFilterChain resourceServerFilterChain(@NotNull HttpSecurity http) throws Exception {
         return http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers(WHITE_LIST).permitAll();
