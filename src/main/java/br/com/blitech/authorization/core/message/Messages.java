58c008c07e32d108fc4a01518fa8148a467cc93b
Carlos Beleli <beleli@gmail.com>
2025-06-25 09:32:40 -0300

    ajustes de internacionalização

--- src/main/java/br/com/blitech/authorization/core/message/Messages.java
+++ src/main/java/br/com/blitech/authorization/core/message/Messages.java
@@ -1,16 +1,21 @@
 package br.com.blitech.authorization.core.message;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.text.MessageFormat;
 import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class Messages {
-    private static final ResourceBundle resourceBundle = ResourceBundle.getBundle("i18n/messages");
+
+    private Messages() {
+        throw new IllegalStateException("Utility class");
+    }
 
     public static String get(String key, @NotNull Object... args) {
         try {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle("i18n/messages", LocaleContextHolder.getLocale());
             String message = resourceBundle.getString(key);
             if (args.length > 0) {
                 return MessageFormat.format(message, args);
